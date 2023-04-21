f7c2ce10dd1246fde2dec0f1be56337159e83bf9
Sean Hendrick <seanhendrick05@gmail.com>
2023-04-21 10:28:44 +0100

    Add Microsoft.Data.SQLite dependency

--- Database.cs
+++ Database.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
 
 namespace Coursework
 {
     internal static class Database
     {
+        internal const string dbPath = "database.db";
     }
 }
